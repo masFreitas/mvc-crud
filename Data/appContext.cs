76f5a60f49df91e70183c1d338ab5607bce8814d
Mateus GitHub <mateusasfreitas@gmail.com>
2021-03-22 19:07:52 -0300

    first commit

---
+++ Data/appContext.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using mvc_crud.Models.DB;
+
+#nullable disable
+
+namespace mvc_crud.Data
+{
+    public partial class appContext : DbContext
+    {
+        public DbSet<Pessoa> Pessoas { get; set; }
+        public appContext()
+        {
+        }
+
+        public appContext(DbContextOptions<appContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlite("Data Source=app.db");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
