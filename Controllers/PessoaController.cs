38c8990b084a0a2fcaa07b5c5599245c80f01135
Mateus GitHub <mateusasfreitas@gmail.com>
2021-03-22 22:04:43 -0300

    Aula 22.03.2021 - Jeito fácil de criar as coisas

--- Controllers/PessoaController.cs
+++ Controllers/PessoaController.cs
@@ -1,41 +1,153 @@
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using mvc_crud.Data;
+using System;
 using System.Collections.Generic;
-using mvc_crud.Models.DB;
 using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using mvc_crud.Data;
+using mvc_crud.Models.DB;
 
 namespace mvc_crud.Controllers
 {
     public class PessoaController : Controller
     {
-        private readonly ILogger<PessoaController> _logger;
         private readonly appContext _context;
 
-        public PessoaController(ILogger<PessoaController> logger, appContext context)
+        public PessoaController(appContext context)
         {
-            _logger = logger;
             _context = context;
         }
 
-        public IActionResult Index()
+        // GET: Pessoa
+        public async Task<IActionResult> Index()
         {
-            IList<Pessoa> pessoas = _context.Pessoas.ToList();
-            return View(pessoas);
+            return View(await _context.Pessoas.ToListAsync());
         }
 
-        public IActionResult Novo()
+        // GET: Pessoa/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var pessoa = await _context.Pessoas
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (pessoa == null)
+            {
+                return NotFound();
+            }
+
+            return View(pessoa);
+        }
+
+        // GET: Pessoa/Create
+        public IActionResult Create()
         {
             return View();
         }
 
+        // POST: Pessoa/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
-        public IActionResult Novo([Bind("Id,Nome,Nascimento,Email")]Pessoa novaPessoa)
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Nome,Nascimento,Email")] Pessoa pessoa)
         {
-            _context.Pessoas.Add(novaPessoa);
-            _context.SaveChanges();
-            
+            if (ModelState.IsValid)
+            {
+                _context.Add(pessoa);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(pessoa);
+        }
+
+        // GET: Pessoa/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var pessoa = await _context.Pessoas.FindAsync(id);
+            if (pessoa == null)
+            {
+                return NotFound();
+            }
+            return View(pessoa);
+        }
+
+        // POST: Pessoa/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Nome,Nascimento,Email")] Pessoa pessoa)
+        {
+            if (id != pessoa.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(pessoa);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PessoaExists(pessoa.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(pessoa);
+        }
+
+        // GET: Pessoa/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var pessoa = await _context.Pessoas
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (pessoa == null)
+            {
+                return NotFound();
+            }
+
+            return View(pessoa);
+        }
+
+        // POST: Pessoa/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var pessoa = await _context.Pessoas.FindAsync(id);
+            _context.Pessoas.Remove(pessoa);
+            await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
+        }
+
+        private bool PessoaExists(int id)
+        {
+            return _context.Pessoas.Any(e => e.Id == id);
         }
     }
 }
